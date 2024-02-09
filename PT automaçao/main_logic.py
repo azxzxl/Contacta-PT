fc5a2f3d65ee0e1fe88e1c50ab26a5d588881944
AzazeL <130715494+azxzxl@users.noreply.github.com>
2024-02-09 00:00:02 -0300

    Update main_logic.py

--- PT automaçao/main_logic.py
+++ PT automaçao/main_logic.py
@@ -1,4 +1,4 @@
-#main_logic.py
+# main_logic.py
 
 from docx import Document
 
@@ -39,41 +39,3 @@ def substituir_texto_no_documento_e_preencher_tabelas(caminho_do_template, subst
             tabela_servicos.cell(i, j).text = str(cell_data)
 
     doc.save(caminho_do_template.replace('.docx', '_modificado.docx'))
-
-
-# Caminho do seu documento template
-caminho_do_template = r'C:\Users\Lucas Rangel\Desktop\Contacta\Conteudo Contacta\TESTEPT.docx'
-
-# Dicionário com as substituições a serem feitas
-substituicoes = {
-    "[fabricante]": "FABRICANTE DESEJADO",
-    "[solução]": "EX DE SOLUÇAO",
-    "[documento]": "DOC AQ",
-    "[serviços]": "SERVIÇO AQ",
-    "[Produto]": "PRODUTO AQ",
-    "[produto]": "PRODUTO AQ",
-    "[Texto descritivo]": "Descritivo do produto"
-}
-
-# Dados para preencher a tabela de controle
-dados_para_tabela_controle = [
-    ('1.0', '2024-02-07', 'Primeira versão do documento.'),
-    # Adicione mais linhas conforme necessário
-]
-
-# Dados para preencher a tabela de produtos nas células vazias
-dados_para_tabela_produtos = [
-    ('Novo Part Number 1', 'Nova Descrição 1', 'Nova Quantidade 1'),
-    ('Novo Part Number 2', 'Nova Descrição 2', 'Nova Quantidade 2'),
-    # Adicione mais dados conforme necessário
-]
-
-# Dados para preencher a tabela de serviços
-dados_para_tabela_servicos = [
-    ('1', 'Descrição do serviço 1', 'Part Number 1', '5'),
-    # Adicione mais linhas conforme necessário
-]
-
-# Chamada da função com os dados fornecidos
-substituir_texto_no_documento_e_preencher_tabelas(
-    caminho_do_template, substituicoes, dados_para_tabela_controle, dados_para_tabela_produtos, dados_para_tabela_servicos)
